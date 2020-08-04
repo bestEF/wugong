060dbe21ea59696fdb96557f5b230fa3a3e09ba0
付建浩 <1562106220@qq.com>
2020-08-04 12:21:57 +0800

    xxx

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -5,5 +5,6 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("1111111111111111");
 		System.out.println("3333333333333333");
+		System.out.println("xxxxxxxxxxxxxxxxxxxx");
 	}
 }
